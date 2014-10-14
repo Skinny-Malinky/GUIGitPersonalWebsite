2ee2b6bd50d5aac8055dbbeaf7defd4ba0463add
Skinny-Malinky <struan.fraser@gmail.com>
2014-10-14 05:19:32 +0100

    Deleted unnecessary files and added to robots.txt

--- old/parse.php
+++
@@ -1,7 +0,0 @@
-<?php
-$xml = simplexml_load_file("https://api.twitter.com/1/lists/statuses.xml?slug=design-students&owner_screen_name=studentdesigner");
-foreach($xml->children() as $child)
-	{
-	echo $child . "<br />";
-	}
-?>
