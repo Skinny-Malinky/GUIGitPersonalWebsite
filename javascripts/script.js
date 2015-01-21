8bd55c88aed67cb5702e91b287422c620b07f29c
Skinny-Malinky <struan.fraser@gmail.com>
2015-01-21 01:37:35 +0000

    New facade, removed header, added animation.
    I've added a facade page for an introduction before having my work
    thrown in your face. Got lots of nice wee JQuery animations. It's
    starting to look much better.

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,17 +4,25 @@ Author: Struan Fraser
 $(document).ready(function () {
     $('.ajax-popup-link').magnificPopup({
         type: 'ajax',
-        
+
         fixedContentPos: false,
         fixedBgPos: true,
-        
+
         overflowY: 'auto',
-        
-        closeBtnInside:true,
-        preloader:false,
-        
-        midClick:true,
+
+        closeBtnInside: true,
+        preloader: false,
+
+        midClick: true,
         removalDelay: 300,
-        mainClass:'mfp-fade'
+        mainClass: 'mfp-fade'
     });
+    $('.fader').click(function (disappear) {
+        $('#intro').fadeOut(400, portFade());
+    });
+
+    function portFade(){
+        $('.content').fadeIn();
+        //#topbar
+    };
 });
